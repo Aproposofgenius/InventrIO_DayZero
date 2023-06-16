0a5f1ec74c1e7ef20b127ae046e6fbb779118ee4
Chloe Eimunjeze <45670453+Aproposofgenius@users.noreply.github.com>
2023-06-16 09:37:16 -0500

    Add Bad wiring system
    Add code for bad wiring system lesson
    Add similar file to have the LED stop blinking after 10 iterations

---
+++ day0/ch1/ch1_blinkingLights_noLoop/ch1_blinkingLights_noLoop.ino
@@ -0,0 +1,35 @@
+/*
+ *
+ * Day 0 - AI Apocalypse by inventr.io
+ * Learn more at https://inventr.io/PLACEHOLDER
+ *
+ * A light-emitting diode (LED) is a semiconductor device that emits light
+ * when an electric current is passed through it. LEDs are small, efficient,
+ * and long-lasting, making them ideal for a wide range of applications such
+ * as indicator lights, digital displays, and lighting. They are available
+ * in a variety of colors and sizes.
+ *
+ * Alex Eschenauer
+ * David Schmidt
+ */
+
+#include "Arduino.h"
+
+const int LIGHT = 22;  // Set "LIGHT" to the HERO XL pin connected to the LED cathode (+) lead
+
+void setup() {
+  // initialize digital pin defined by "LIGHT" as an output pin.
+  pinMode(LIGHT, OUTPUT);
+  //add loop into setup to run only once
+  for (int x = 0; x < 10; x++) { //only do this 10 times
+    digitalWrite(LIGHT, HIGH);  // Set our designated LIGHT pin to HIGH value (5V), or "on"
+    delay(2000);                // Wait for 2 second (2000 microseconds)
+
+    digitalWrite(LIGHT, LOW);  // Now set our pin to LOW voltage (0V), or "off".
+    delay(1000);               // Wait one second.
+  }
+}
+
+void loop() {
+
+} 
