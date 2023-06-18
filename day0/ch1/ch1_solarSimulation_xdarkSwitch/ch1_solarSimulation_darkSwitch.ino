b60797a2f9374eca51e9a727689c97056f740ae7
Chloe Eimunjeze <45670453+Aproposofgenius@users.noreply.github.com>
2023-06-18 13:58:24 -0500

    Solar Simulation
    Adding code for solar simulation and some file structure clean up

---
+++ day0/ch1/ch1_solarSimulation_xdarkSwitch/ch1_solarSimulation_darkSwitch.ino
@@ -0,0 +1,83 @@
+/*
+ * Day 0 - AI Apocalypse by inventr.io
+ * Learn more at https://inventr.io/PLACEHOLDER
+ *
+ *Side project based off the solar simulation lesson
+ * the project generally turn the led light on when it gets dark or its room level
+ * if its bright turn off
+ */
+
+#include "Arduino.h"
+
+const uint8_t LIGHT = 22;          // LED on pin 22
+const uint8_t LIGHT_BUTTON = 23;   // Button (light switch) on pin 23
+const uint8_t PHOTO_SENSOR = A8;  // Photoresistor input simulating battery charge rate
+
+const uint8_t PRESSED = LOW;       // Button input pin reads LOW when pressed
+const uint8_t NOT_PRESSED = HIGH;  // Button input pin reads HIGH when NOT pressed
+const uint8_t ON = HIGH;           // Lights are ON when pin is HIGH
+const uint8_t OFF = LOW;           // Lights are OFF when pin is LOW
+
+const float HIGH_INTENSITY_LIMIT = 300;  //Average room photo intensity, adjust depending on the brightness you want the light to turn off (0-1023)
+
+
+void setup() {
+  // Intialize Serial class, used to communicate with the Arduino IDE Serial Monitor
+  Serial.begin(9600);  // Initialize Serial, set speed to 9600 bits/second (baud)
+  while (!Serial) {
+    ;  // wait for serial port to connect.
+  }
+  pinMode(LIGHT, OUTPUT);               // LED representing our light (output)
+  pinMode(LIGHT_BUTTON, INPUT_PULLUP);  // Button controlling light (input with pull-up resistor)
+  pinMode(PHOTO_SENSOR, INPUT);        // Photoresistor analog input
+}
+
+
+bool light_on = false;                     // we start with the light turned off
+bool previous_button_state = NOT_PRESSED;  // start out with button NOT pressed
+
+void loop() {
+  int current_intensity = analogRead(PHOTO_SENSOR);  // Read from our photoresistor (0-1023)
+
+  // If out light on and its bright, turn light off, if its dark turn it on
+  if (light_on && current_intensity > HIGH_INTENSITY_LIMIT) {
+    digitalWrite(LIGHT, OFF);  // Light is on, turn it off
+    light_on = false;          // ... and save it's new state
+  }  else if (!light_on && current_intensity < HIGH_INTENSITY_LIMIT) {
+    digitalWrite(LIGHT, ON);  // Light is on, turn it off
+    light_on = true;          // ... and save it's new state
+  }
+
+
+  // Output the numbers we wish to plot using the Serial Plotter.  The first two numbers
+  // are just to show the 0% and 100% charged points so the plotter won't continuously
+  // change the scale.
+  Serial.print(0);    // show line in plotter for 0% charge
+  Serial.print(", ");
+  Serial.print(100);  // show line in plotter for 100% charge
+  Serial.print(", ");
+  Serial.print(current_intensity);  // show current intensity in percent
+  Serial.print(", ");
+  Serial.println(map(current_intensity, 0, 1023, 0, 100));  
+
+  // =========== Second part of loop is our prior button / LED control
+
+  // Since we only use the button state *inside* loop() we declare it here as a local variable.
+  uint8_t button_state = digitalRead(LIGHT_BUTTON);  // read current button state and save it
+
+  // first check to see if the button state has changed since last loop()
+  if (button_state != previous_button_state) {
+    if (button_state == PRESSED) {  // if our NEW state is PRESSED this is a new button press
+      // then toggle our light, turning it of if it's on, and on if it's off.
+      if (light_on) {
+        digitalWrite(LIGHT, LOW);   // Light is on, turn it off
+        light_on = false;           // ... and save it's new state
+      } else {                      // Light must be off
+        digitalWrite(LIGHT, HIGH);  // turn on light
+        light_on = true;            // ... and save it's new state
+      }
+    }
+    // Since button state changed, let's save its current state for next time through loop()
+    previous_button_state = button_state;
+  }
+}
